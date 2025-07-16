45a4eabd38d24ceaf058b0ed096b356c1661173e
Tyler Wagner <TWagner2912@gmail.com>
2025-07-16 22:34:19 +0200

    woche2 praktikum

--- ip2025/praktikum/woche2/aufgabe3.c
+++ ip2025/praktikum/woche2/aufgabe3.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int print_array(int array, int length) {
-
+int print_array(int array[], int length) {
+    for(int i = 0; i <= length; i++) {
+        printf("%d, ", array[i]);
+    }
 }
 
 int main(int argc, char const *argv[])
