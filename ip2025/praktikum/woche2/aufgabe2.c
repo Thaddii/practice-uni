c2d660a5db34ba141af2b2f4143539692b94c214
Tyler Wagner <TWagner2912@gmail.com>
2025-07-22 20:04:52 +0200

    rename praktikum folders

--- ip2025/praktikum/woche2/aufgabe2.c
+++ ip2025/praktikum/woche2/aufgabe2.c
@@ -1,46 +1,18 @@
 #include <stdio.h>
-#include <math.h>
-
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
 
 int main(int argc, char const *argv[])
 {
-    double x;
-    int i;
-    float sin_von_x;
-    float cos_von_x;
-    int sin_indent;
-    int cos_indent;
+    int n;
+    int f = 1;
+    int i = 1;
 
-    // Intervall -180 Grad bis 180 Grad in 15er Schritten
-    for(x = -180.0; x <= 180.0; x += 15.0) {
-        // sin(x / 180.0 * M_PI);
-        // Sinus Wert für x ausrechenen inkl. Umrechnung von x in Radians, da sin() von der Einheit Radian ausgeht
-        // sin() liefert einen Wert zwischen -1 und 1
-        sin_von_x = sin(x * (M_PI / 180.0));
-        cos_von_x = cos(x * (M_PI / 180.0));
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
-        // Multipliziert mit 10 um den Wertebereich auf die Plotterzeilen abzubilden
-        sin_indent = sin_von_x * 10;
-        cos_indent = cos_von_x * 10;
-
-        // Wert um 10 erhöt (Wertebereich von -10 bis 10 auf 0 bis 20 verschoben)
-        sin_indent += 10;
-        cos_indent += 10;
-
-        // Plotterzeile 21 Zeichen lang
-        for(i = 0; i <= 20; i++) {
-            if(i == sin_indent && i == cos_indent) {
-                putchar('*');
-            } else if(i == sin_indent) {
-                putchar('+');
-            } else if(i == cos_indent) {
-                putchar('x');
-            } else putchar(' ');
-        }
-        putchar('\n');
+    while(i <= n) {
+        f = f * i;
+        i = i + 1;
     }
-    return 0;
+    printf("The result of f: %d\n", f); // n = 0 == 4 && n = 4 == 24
+
 }
--- ip2025/praktikum/woche2/aufgabe3.c
+++ ip2025/praktikum/woche2/aufgabe3.c
@@ -1,59 +1,64 @@
 #include <stdio.h>
 
-int print_array(int array[], int length) {
-    for(int i = 0; i <= length; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-    return 0;
-}
-
-int array_sum(int array[], int length) {
-    int result = 0;
-    for(int i = 0; i <= length; i++) {
-        result += array[i];
-    }
-    return result;
-}
-
-int array_max(int array[], int length){
-    int highest_num = 0;
-    for(int i = 0; i <= length; i++) {
-        if(array[i] > highest_num) {
-            highest_num = array[i];
-        }
-    }
-    return highest_num;
-}
-
-int array_min(int array[], int length) {
-    int lowest_num = 1000;
-    for(int i = 0; i <= length; i++) {
-        if(array[i] < lowest_num) lowest_num = array[i];
-    }
-    return lowest_num;
-}
-
-float array_average(int array[], int length) {
-    float sum = 0;
-    for(int i = 0; i <= length; i++) {
-        sum += array[i];
-    }
-    float average = (sum / (length + 1));
-    return average;
-}
-
 int main(int argc, char const *argv[])
 {
-    int numbers[] = {2, 4, 6, 8};
-    print_array(numbers, 3);
+    // 1.
+    for(int i = 1; i <= 10; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
 
-    printf("%d\n", array_sum(numbers, 3));
+    /*
+    int i = 1;
+    while(i <= 10) {
+        printf("%d ", i);
+        i++;
+    }
+    printf("\n");
+    */
 
-    printf("%d\n", array_max(numbers, 3));
+    // 2.
+    for(int i = 1; i <= 5; i++) {
+        if(i % 2 == 0) {
+            printf("%d - gerade\n", i);
+        }
+        else {
+            printf("%d - ungerade\n", i);
+        }
+    }
 
-    printf("%d\n", array_min(numbers, 3));
+    // 3.
+    for(int i = 1; i <= 100; i++) {
+        if(i != 22) {
+            printf("%d\n", i);
+        }
+    }
 
-    printf("%.2f\n", array_average(numbers, 3));
+    // 4.
+    for(int i = 1; i <= 10; i++) {
+        for(int j = 1; j <= 10; j++) {
+            int ergebnis = i * j;
+            printf("%d * %d = %d \n", i, j, ergebnis);
+        }
+        printf("\n");
+    }
+
+    // 5.
+    int num1 = 1;
+    do {
+        printf("Enter a number: ");
+        scanf("%d", &num1);
+
+        if(num1 % 2 == 0) {
+            printf("%d - gerade\n", num1);
+        }
+        else {
+            printf("%d - ungerade\n", num1);
+        }
+    } while (num1 != 0);
+
+    // 6.
+    // Primzahlen
+
     return 0;
 }
