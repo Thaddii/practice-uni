3f377f6aabf25f6f1c3d515d23aab0f18bb2828c
Tyler Wagner <TWagner2912@gmail.com>
2025-07-26 15:45:04 +0200

    woche4

--- ip2025/praktikum/woche4/1d_arrays.c
+++ ip2025/praktikum/woche4/1d_arrays.c
@@ -3,19 +3,20 @@
 int main(int argc, char const *argv[])
 {
     int arr_2D[3][5] = {{-1, 12, -3, 2, 1}, {0, 0, 1, 2, 3}, {5, 5, -5, -6, 6}};
-    return 0;
 
     int arr_1D[15];
     int index = 0;
 
     for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
+        for(int j = 0; j < 5; j++) {
             arr_1D[index++] = arr_2D[i][j];
         }
     }
 
     for(int i = 0; i < 15; i++) {
-        printf("%d", arr_1D[i]);
+        printf("%d ", arr_1D[i]);
     }
     printf("\n");
+
+    return 0;
 }
